893193cc048d02a3cb537ffc825e64e27e673670
maare <maarekarmen@gmail.com>
2026-04-18 22:45:33 +0300

    add fk to db init

--- backend/db/create_db.sql
+++ backend/db/create_db.sql
@@ -1,53 +1,31 @@
 CREATE TABLE TRANSLATIONS (
 	id SERIAL PRIMARY KEY,
-	author int,
-	translator int,
-	editor int,
-	fore_afterword_author int,
 	title varchar(255) NOT NULL,
 	title_original varchar(255),
-	genre int,
-	source_language int,
-	source_literature int,
-	publisher int,
-	publication_location int,
+	publisher int, --fk
 	publication_year int NOT NULL,
 	physical_description varchar(255),
-	series int,
-	publication_type int,
-	target_audience int,
+	series int, --fk
+    target_audience int, --dk
 	edition varchar(255),
 	n_pages varchar(64),
 	content varchar(255),
 	issue varchar(255),
-	notes varchar(255)
+	notes varchar(255),
+
+    CONSTRAINT fk_publisher
+    FOREIGN KEY (publisher)
+    REFERENCES PUBLISHERS(id),
+
+    CONSTRAINT fk_series
+    FOREIGN KEY (series)
+    REFERENCES SERIES(id),
+
+    CONSTRAINT fk_target_audience
+    FOREIGN KEY (target_audience)
+    REFERENCES TARGET_AUDIENCES(id)
 );
 
-CREATE TABLE REVIEWS (
-	id SERIAL PRIMARY KEY,
-	translation_id int,
-	author int,
-	translator int,
-	editor int,
-	fore_afterword_author int,
-	title varchar(255) NOT NULL,
-	title_original varchar(255),
-	genre int,
-	source_language int,
-	source_literature int,
-	publisher int,
-	publication_location int,
-	publication_year int NOT NULL,
-	physical_description varchar(255),
-	series int,
-	publication_type int,
-	target_audience int,
-	edition varchar(255),
-	n_pages varchar(255),
-	content varchar(255),
-	issue varchar(255),
-	notes varchar(255)
-); -- future work: review table should not contain data on translated book, that should be in translations
 
 CREATE TABLE PERSONS (
 	id SERIAL PRIMARY KEY,
@@ -56,10 +34,18 @@ CREATE TABLE PERSONS (
 );
 
 CREATE TABLE NAMES (
-	person_id int,
-	language_id int,
+	person_id int, --fk
+	language_id int, --fk
 	first_name varchar(255),
-	last_name varchar(255)
+	last_name varchar(255),
+
+    CONSTRAINT fk_person_id
+    FOREIGN KEY (person_id)
+    REFERENCES PERSONS(id),
+
+    CONSTRAINT fk_language_id
+    FOREIGN KEY (language_id)
+    REFERENCES LANGUAGES(id)
 );
 
 CREATE TABLE LANGUAGES (
@@ -112,92 +98,150 @@ CREATE TABLE PUBLICATION_TYPES (
 -- Translations
 CREATE TABLE TRANSLATIONS_AUTHOR_PERSONS (
     translation_id int NOT NULL,
-    person_id int NOT NULL
+    person_id int NOT NULL,
+
+    PRIMARY KEY (translation_id, person_id),
+
+    CONSTRAINT fk_translation_id
+    FOREIGN KEY (translation_id)
+    REFERENCES TRANSLATIONS(id),
+
+    CONSTRAINT fk_person_id
+    FOREIGN KEY (person_id)
+    REFERENCES PERSONS(id)
 );
 
 CREATE TABLE TRANSLATIONS_TRANSLATOR_PERSONS (
     translation_id int NOT NULL,
-    person_id int NOT NULL
+    person_id int NOT NULL,
+
+    PRIMARY KEY (translation_id, person_id),
+
+    CONSTRAINT fk_translation_id
+    FOREIGN KEY (translation_id)
+    REFERENCES TRANSLATIONS(id),
+
+    CONSTRAINT fk_person_id
+    FOREIGN KEY (person_id)
+    REFERENCES PERSONS(id)
 );
 
 CREATE TABLE TRANSLATIONS_EDITOR_PERSONS (
     translation_id int NOT NULL,
-    person_id int NOT NULL
+    person_id int NOT NULL,
+
+    PRIMARY KEY (translation_id, person_id),
+
+    CONSTRAINT fk_translation_id
+    FOREIGN KEY (translation_id)
+    REFERENCES TRANSLATIONS(id),
+
+    CONSTRAINT fk_person_id
+    FOREIGN KEY (person_id)
+    REFERENCES PERSONS(id)
 );
 
-CREATE TABLE TRANSLATIONS_COMMENT_PERSONS (
+CREATE TABLE TRANSLATIONS_FORE_AFTERWORD_PERSONS (
     translation_id int NOT NULL,
-    person_id int NOT NULL
+    person_id int NOT NULL,
+
+    PRIMARY KEY (translation_id, person_id),
+
+    CONSTRAINT fk_translation_id
+    FOREIGN KEY (translation_id)
+    REFERENCES TRANSLATIONS(id),
+
+    CONSTRAINT fk_person_id
+    FOREIGN KEY (person_id)
+    REFERENCES PERSONS(id)
+);
+
+CREATE TABLE TRANSLATIONS_REVIEWED_BOOK_AUTHORS_PERSONS (
+    translation_id int NOT NULL,
+    person_id int NOT NULL,
+
+    PRIMARY KEY (translation_id, person_id),
+
+    CONSTRAINT fk_translation_id
+    FOREIGN KEY (translation_id)
+    REFERENCES TRANSLATIONS(id),
+
+    CONSTRAINT fk_person_id
+    FOREIGN KEY (person_id)
+    REFERENCES PERSONS(id)
 );
 
 CREATE TABLE TRANSLATIONS_LANGUAGES (
     translation_id int NOT NULL,
-    language_id int NOT NULL
+    language_id int NOT NULL,
+
+    PRIMARY KEY (translation_id, language_id),
+
+    CONSTRAINT fk_translation_id
+    FOREIGN KEY (translation_id)
+    REFERENCES TRANSLATIONS(id),
+
+    CONSTRAINT fk_languages_id
+    FOREIGN KEY (language_id)
+    REFERENCES LANGUAGES(id)
 );
 
 CREATE TABLE TRANSLATIONS_GENRES (
     translation_id int NOT NULL,
-    language_id int NOT NULL
+    genre_id int NOT NULL,
+
+    PRIMARY KEY (translation_id, genre_id),
+
+    CONSTRAINT fk_translation_id
+    FOREIGN KEY (translation_id)
+    REFERENCES GENRES(id),
+
+    CONSTRAINT fk_person_id
+    FOREIGN KEY (genre_id)
+    REFERENCES GENRES(id)
 );
 
 CREATE TABLE TRANSLATIONS_LITERATURES (
     translation_id int NOT NULL,
-    language_id int NOT NULL
-);
+    literature_id int NOT NULL,
 
+    PRIMARY KEY (translation_id, literature_id),
+
+    CONSTRAINT fk_translation_id
+    FOREIGN KEY (translation_id)
+    REFERENCES TRANSLATIONS(id),
+
+    CONSTRAINT fk_literature_id
+    FOREIGN KEY (literature_id)
+    REFERENCES LITERATURES(id)
+);
 
 CREATE TABLE TRANSLATIONS_LOCATIONS (
     translation_id int NOT NULL,
-    language_id int NOT NULL
+    location_id int NOT NULL,
+
+    PRIMARY KEY (translation_id, location_id),
+
+    CONSTRAINT fk_translation_id
+    FOREIGN KEY (translation_id)
+    REFERENCES TRANSLATIONS(id),
+
+    CONSTRAINT fk_location_id
+    FOREIGN KEY (location_id)
+    REFERENCES LOCATIONS(id)
 );
 
 CREATE TABLE TRANSLATIONS_PUBLICATION_TYPES (
     translation_id int NOT NULL,
-    language_id int NOT NULL
-);
+    publication_type_id int NOT NULL,
 
---- Reviews
-CREATE TABLE REVIEWS_AUTHOR_PERSONS (
-    review_id int NOT NULL,
-    person_id int NOT NULL
-);
+    PRIMARY KEY (translation_id, publication_type_id),
 
-CREATE TABLE REVIEWS_TRANSLATOR_PERSONS (
-    review_id int NOT NULL,
-    person_id int NOT NULL
-);
+    CONSTRAINT fk_translation_id
+    FOREIGN KEY (translation_id)
+    REFERENCES TRANSLATIONS(id),
 
-CREATE TABLE REVIEWS_EDITOR_PERSONS (
-    review_id int NOT NULL,
-    person_id int NOT NULL
-);
-
-CREATE TABLE REVIEWS_COMMENT_PERSONS (
-    review_id int NOT NULL,
-    person_id int NOT NULL
-);
-
-CREATE TABLE REVIEWS_LANGUAGES (
-    review_id int NOT NULL,
-    language_id int NOT NULL
-);
-
-CREATE TABLE REVIEWS_GENRES (
-    review_id int NOT NULL,
-    language_id int NOT NULL
-);
-
-CREATE TABLE REVIEWS_LITERATURES (
-    review_id int NOT NULL,
-    language_id int NOT NULL
-);
-
-CREATE TABLE REVIEWS_LOCATIONS (
-    review_id int NOT NULL,
-    language_id int NOT NULL
-);
-
-CREATE TABLE REVIEWS_PUBLICATION_TYPES (
-    review_id int NOT NULL,
-    language_id int NOT NULL
+    CONSTRAINT fk_publication_type_id
+    FOREIGN KEY (publication_type_id)
+    REFERENCES PUBLICATION_TYPES(id)
 );
